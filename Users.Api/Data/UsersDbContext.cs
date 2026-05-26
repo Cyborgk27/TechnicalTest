f496d72f1553fdce8a5576584cbecf0a930f815b
kcepeda <slenderstalin@hotmail.com>
2026-05-26 14:37:16 -0500

    chore: se agrega el dbcontext

---
+++ Users.Api/Data/UsersDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using Users.API.Models;
+
+namespace Users.API.Data
+{
+    /// <summary>
+    /// Contexto de Entity Framework Core para la gestión de usuarios en SQL Server.
+    /// </summary>
+    public class UsersDbContext : DbContext
+    {
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Colección de datos de los usuarios en la base de datos.
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+        }
+    }
+}
